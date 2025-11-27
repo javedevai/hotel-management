f997b2e667aa109d5458fc703d36510685a09c15
javedevai <7javed@proton.me>
2025-11-27 11:24:07 +0500

    fix

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ejqozm.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqcW96bXB0ZHdkYm6MjA3OTc5ODkzN30.DJA1JH2ChStxzukWZjiajWZu1TnYuHDbo51V';
+const supabaseUrl = 'https://ejqozmptdwdbkbazxzyk.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqcW96bXB0ZHdkYmtiYXp4enlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyMjI5MzcsImV4cCI6MjA3OTc5ODkzN30.DJA1JH2ChStxzukWZjiajWZu1TnYuHDbo51V_h9_puM';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
